f1de38df23180315e2b3bb696daa07f0ed6628f7
Nathan Caron <caron.nathan@hotmail.com>
2022-09-13 18:09:10 +0200

    Chap: First page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+
+  def home
+  end
+
+  def salut
+    @name = params[:name]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root 'pages#home'
+
+  get '/bonjour(/:name)', to: 'pages#salut', as: 'salut'
 end
