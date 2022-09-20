6fc64185bfc90fd06a71eb402c097a5278b3c36d
Nathan Caron <caron.nathan@hotmail.com>
2022-09-20 14:19:50 +0200

    Chap: Controller, formats

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,18 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+    end
   end
 
   def show
+    respond_to do |format|
+      format.html
+      format.json { render json: @post }
+    end
   end
 
   def edit
