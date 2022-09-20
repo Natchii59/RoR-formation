9ff573c3fe62ca243291390aa0c5a9b82faa3be6
Nathan Caron <caron.nathan@hotmail.com>
2022-09-20 17:05:39 +0200

    Chap: Models, scopes

--- app/models/Post.rb
+++ app/models/Post.rb
@@ -2,6 +2,9 @@ class Post < ApplicationRecord
 
   include Sluggable
 
+  default_scope { order(created_at: :desc) }
+  scope :online, -> (online = 1) { where(online: online) }
+
   def as_json(options = nil)
     super(only: [:id, :name, :created_at])
   end
